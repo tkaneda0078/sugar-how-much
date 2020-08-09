eeeeedc86e6e3456b23477a2060156eb1ab2de2f
金田尚大 <1111510078r21@gmail.com>
2020-08-09 15:25:34 +0900

    計算修正

--- lib/src/resources/helpers/calculation_result_view_helper.dart
+++ lib/src/resources/helpers/calculation_result_view_helper.dart
@@ -3,7 +3,7 @@ class CalculationResultViewHelper {
   getSugarDegreeText(double sugarValue) {
     if (sugarValue <= 6.5) {
       return '低い◎';
-    } else if (sugarValue >= 6.6 || sugarValue <= 8.6) {
+    } else if (sugarValue >= 6.6 && sugarValue <= 8.6) {
       return '普通△';
     } else {
       /// 糖質8.7g以上
